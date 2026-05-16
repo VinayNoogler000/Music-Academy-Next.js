c95bba397e6f60020888a7674cdeb7c2ead4f6f7
VinayNoogler000 <vinaytambey000@gmail.com>
2026-05-16 14:08:14 +0530

    Add "Instructors" section to the homepage with "animated-tooltip" and "wavy-background" UI components from Aceternity UI Library. Also added "simplex-noise" package for the wavy background effect. And updated Next.js config to allow images (only) from Unsplash and Cloudinary.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
 import Testimonials from "@/components/Testimonials";
 import UpcomingWebninars from "@/components/UpcomingWebninars";
 import WhyChooseUs from "@/components/WhyChooseUs";
@@ -13,6 +14,7 @@ export default function Home() {
       <WhyChooseUs />
       <Testimonials />
       <UpcomingWebninars />
+      <Instructors />
     </main>
   );
 }
