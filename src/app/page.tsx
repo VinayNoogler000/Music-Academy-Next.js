3eee0cc2de1720e894de3c84515a28f166848e01
VinayNoogler000 <vinaytambey000@gmail.com>
2026-05-17 15:41:45 +0530

    Update homepage title formatting for Vinay Tambey and Chai Aur Code links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ import WhyChooseUs from "@/components/WhyChooseUs";
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/96 antialiased bg-grid-white/[0.02]">
-      <h1 className="text-2xl text-center"><a href="https://www.vinaytambey.bio" target="_blank" className="border-white border-b-2">Vinay Tambey</a> x <a href="https://youtube.com/playlist?list=PLu71SKxNbfoDWGIwaEwhTUR40AbH8qsTo&si=0fc4XUV_BSY5LbMU" target="_blank" className="border-white border-b-2">Chai Aur Code</a></h1>
+      <h1 className="text-sm text-center"><a href="https://youtube.com/playlist?list=PLu71SKxNbfoDWGIwaEwhTUR40AbH8qsTo&si=0fc4XUV_BSY5LbMU" target="_blank" className="border-white border-b-2">Chai Aur Code</a> &nbsp; x &nbsp; <a href="https://www.vinaytambey.bio" target="_blank" className="border-white border-b-2">Vinay Tambey</a></h1>
       <HeroSection/>
       <FeaturedCourses/>
       <WhyChooseUs />
